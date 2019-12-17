9a1f1422f460152a88f3e551d6989d9a8580788a
peaceful-distribution-8695 <porcaoma.1357@gmail.com>
2019-12-17 08:21:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ star_wars = ["Obi_wan", "Anakin", "Darth Vader", "Luke Skywalker"]
 end
 
 def add_element_to_end_of_array(array, element)
-array << "Yoda"
+array << element
 end
 
 def add_element_to_start_of_array(array, element)
